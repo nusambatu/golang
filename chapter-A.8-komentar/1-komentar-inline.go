fa61a8b56306915714e70fd3756e3e633bd7814f
nusambatu <nusambatu@gmail.com>
2020-10-22 11:18:34 +0700

    menambahkan baris program

--- chapter-A.8-komentar/1-komentar-inline.go
+++ chapter-A.8-komentar/1-komentar-inline.go
@@ -6,6 +6,10 @@ func main() {
 	// komentar kode
 	// menampilkan pesan hello world
 	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello selamat datang ")
+	fmt.Println("hello agu gak tahu")
 
 	// fmt.Println("baris ini tidak akan di eksekusi")
 	// fmt.Println("baris ini tidak akan di eksekusi")
