4c07fd61e884105a17540b1139a1d0a7c6fb374e
nusambatu <nusambatu@gmail.com>
2020-10-22 11:42:04 +0700

    menambah program

--- chapter-A.7-hello-world/main.go
+++ chapter-A.7-hello-world/main.go
@@ -5,4 +5,15 @@ import "fmt"
 func main() {
     fmt.Println("hello world")
     fmt.Println("selamat datang para pembelajar")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar1")
+    fmt.Println("selamat datang para pembelajar2")
+  
 }
