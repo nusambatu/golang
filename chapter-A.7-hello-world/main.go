4310203a8c936a361318afbf073730ada3f74dbd
nusambatu <nusambatu@gmail.com>
2020-10-22 11:56:28 +0700

    menambahkan nama kantor

--- chapter-A.7-hello-world/main.go
+++ chapter-A.7-hello-world/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
-    fmt.Println("selamat datang para pembelajar")
-    fmt.Println("selamat datang para pembelajar1")
-    fmt.Println("selamat datang para pembelajar1")
-    fmt.Println("selamat datang para pembelajar1")
-    fmt.Println("selamat datang para pembelajar1")
-    fmt.Println("darwanto st")
-  
+	fmt.Println("hello world")
+	fmt.Println("selamat datang para pembelajar")
+	fmt.Println("selamat datang para pembelajar1")
+	fmt.Println("selamat datang para pembelajar1")
+	fmt.Println("selamat datang para pembelajar1")
+	fmt.Println("selamat datang para pembelajar1")
+	fmt.Println("darwanto st")
+	fmt.Println("di bagian administrasi dan perekonomian ")
+
 }
