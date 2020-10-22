55e123d7b4ef5ae176465dcf883d8e0171f825e8
nusambatu <nusambatu@gmail.com>
2020-10-22 11:07:27 +0700

    Update 1-komentar-inline.go
    menambah line code

--- chapter-A.8-komentar/1-komentar-inline.go
+++ chapter-A.8-komentar/1-komentar-inline.go
@@ -8,4 +8,6 @@ func main() {
 	fmt.Println("hello world")
 
 	// fmt.Println("baris ini tidak akan di eksekusi")
+	// fmt.Println("baris ini tidak akan di eksekusi")
+	// fmt.Println("baris ini tidak akan di eksekusi")
 }
