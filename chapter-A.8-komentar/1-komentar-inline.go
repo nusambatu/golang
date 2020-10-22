2669d404bfbd90f59f78fd48c16f908fd18577d4
nusambatu <nusambatu@gmail.com>
2020-10-22 11:20:50 +0700

    menghabus dan mengedit baris program

--- chapter-A.8-komentar/1-komentar-inline.go
+++ chapter-A.8-komentar/1-komentar-inline.go
@@ -5,11 +5,8 @@ import "fmt"
 func main() {
 	// komentar kode
 	// menampilkan pesan hello world
-	fmt.Println("hello world")
-	fmt.Println("hello world")
-	fmt.Println("hello world")
-	fmt.Println("hello selamat datang ")
-	fmt.Println("hello agu gak tahu")
+	fmt.Println("selmat datang saudar saudara")
+	
 
 	// fmt.Println("baris ini tidak akan di eksekusi")
 	// fmt.Println("baris ini tidak akan di eksekusi")
